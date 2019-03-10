2f14c1b6c0ff609a4959bd265c69cbe7bc4995c3
TakNek <rekhichem@gmail.com>
2019-03-10 23:06:33 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use Framework\App;
+use GuzzleHttp\Psr7\ServerRequest;
+
+require '..\vendor\autoload.php';
+$app = new App();
+$response = $app->run(ServerRequest::fromGlobals());
+\Http\Response\send($response);
